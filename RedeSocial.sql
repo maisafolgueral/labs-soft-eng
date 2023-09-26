ff32afbddbde12e297e4a23f4460c7a50523d173
Matheus Farias de Oliveira Matsumoto <83461020+MatheusFarias03@users.noreply.github.com>
2023-09-26 09:42:42 -0300

    Modificado RedeSocial.sql para o Inglês.

--- RedeSocial.sql
+++ RedeSocial.sql
@@ -1,60 +1,60 @@
-CREATE TABLE "segue_usuario" (
-  "seguidor_id" integer,
-  "seguido_id" integer
+CREATE TABLE "follow_user" (
+  "follower_id" integer,
+  "followed_id" integer
 );
 
-CREATE TABLE "usuario" (
+CREATE TABLE "user" (
   "id" integer PRIMARY KEY,
-  "nome" varchar,
-  "sobrenome" varchar,
-  "sexo" varchar,
+  "name" varchar,
+  "surname" varchar,
+  "gender" varchar,
   "email" varchar,
-  "senha" varchar,
+  "password" varchar,
   "created_at" timestamp
 );
 
-CREATE TABLE "publicacao" (
+CREATE TABLE "post" (
   "id" integer PRIMARY KEY,
-  "id_usuario" integer,
-  "id_topico" integer,
-  "titulo" varchar,
-  "conteudo" text,
-  "data_criacao" timestamp
+  "id_user" integer,
+  "id_topic" integer,
+  "title" varchar,
+  "content" text,
+  "creation_data" timestamp
 );
 
-CREATE TABLE "reacao" (
+CREATE TABLE "reaction" (
   "id" integer PRIMARY KEY,
-  "id_usuario" integer,
-  "id_publicacao" integer,
-  "tipo" varchar
+  "id_user" integer,
+  "id_post" integer,
+  "type" varchar
 );
 
-CREATE TABLE "topico" (
+CREATE TABLE "topic" (
   "id" integer PRIMARY KEY,
-  "assunto" varchar
+  "subject" varchar
 );
 
-CREATE TABLE "segue_topico" (
-  "seguidor_id" integer,
-  "topico_id" integer
+CREATE TABLE "follows_topic" (
+  "follower_id" integer,
+  "topic_id" integer
 );
 
-COMMENT ON COLUMN "publicacao"."id_usuario" IS 'Criador da publicacao';
+COMMENT ON COLUMN "post"."id_user" IS 'Post Creator';
 
-COMMENT ON COLUMN "publicacao"."id_topico" IS 'A qual topico pertence';
+COMMENT ON COLUMN "post"."id_topic" IS 'Which topic belongs to';
 
-ALTER TABLE "publicacao" ADD FOREIGN KEY ("id_usuario") REFERENCES "usuario" ("id");
+ALTER TABLE "post" ADD FOREIGN KEY ("id_user") REFERENCES "user" ("id");
 
-ALTER TABLE "segue_usuario" ADD FOREIGN KEY ("seguidor_id") REFERENCES "usuario" ("id");
+ALTER TABLE "follow_user" ADD FOREIGN KEY ("follower_id") REFERENCES "user" ("id");
 
-ALTER TABLE "segue_usuario" ADD FOREIGN KEY ("seguido_id") REFERENCES "usuario" ("id");
+ALTER TABLE "follow_user" ADD FOREIGN KEY ("followed_id") REFERENCES "user" ("id");
 
-ALTER TABLE "reacao" ADD FOREIGN KEY ("id_usuario") REFERENCES "usuario" ("id");
+ALTER TABLE "reaction" ADD FOREIGN KEY ("id_user") REFERENCES "user" ("id");
 
-ALTER TABLE "reacao" ADD FOREIGN KEY ("id_publicacao") REFERENCES "publicacao" ("id");
+ALTER TABLE "reaction" ADD FOREIGN KEY ("id_post") REFERENCES "post" ("id");
 
-ALTER TABLE "publicacao" ADD FOREIGN KEY ("id_topico") REFERENCES "topico" ("id");
+ALTER TABLE "post" ADD FOREIGN KEY ("id_topico") REFERENCES "topic" ("id");
 
-ALTER TABLE "segue_topico" ADD FOREIGN KEY ("seguidor_id") REFERENCES "usuario" ("id");
+ALTER TABLE "follows_topic" ADD FOREIGN KEY ("follower_id") REFERENCES "user" ("id");
 
-ALTER TABLE "segue_topico" ADD FOREIGN KEY ("topico_id") REFERENCES "topico" ("id");
+ALTER TABLE "follows_topic" ADD FOREIGN KEY ("topic_id") REFERENCES "topic" ("id");
