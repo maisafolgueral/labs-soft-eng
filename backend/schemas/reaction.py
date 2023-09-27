19f25da04e592d612bcee87bd1889f203e83589b
guichiare <64621991+guichiare@users.noreply.github.com>
2023-09-26 22:23:57 -0300

    Create reaction.py

---
+++ backend/schemas/reaction.py
@@ -0,0 +1,14 @@
+'''
+In this file, it is set the
+schema to load and dump data
+in a specified format
+'''
+
+from marshmallow import Schema, fields
+
+class ReactionSchema(Schema):
+
+    id = fields.Integer(dump_only=True)
+    user_id = fields.Integer(dump_only=True)
+    post_id = fields.Integer(dump_only=True)
+    type = fields.String(required=True)
