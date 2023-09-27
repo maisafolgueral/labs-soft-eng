e7e05a1279bc5b857c7797dfb8e757d63d90c53f
guichiare <64621991+guichiare@users.noreply.github.com>
2023-09-26 21:55:13 -0300

    Create follow_user.py

---
+++ backend/models/follow_user.py
@@ -0,0 +1,20 @@
+'''
+In this file, it is set the
+model of a specified class
+for ORM
+'''
+
+from sqlalchemy import Column
+from sqlalchemy.types import Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+import bcrypt
+
+Base = declarative_base()
+
+class FollowUserModel(Base):
+
+    __tablename__ = 'reaction'
+
+    follower_id = Column(Integer, nullable=False)
+    followed_id = Column(Integer, nullable=False)
