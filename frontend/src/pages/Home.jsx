e52d042a4eb48be1d3102a9234115bedfaf0c5b4
maisafolgueral <maisa.folgueral@gmail.com>
2023-10-12 14:38:49 -0300

    Updated Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,23 @@
-import { useNavigate } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import { Main } from '@/components/Styled';
-import isologo from '@/assets/branding/hola-isologo-coloful.svg';
-import bannerHome from '@/assets/images/banner-home.png';
-import footerHome from '@/assets/images/footer-home.png';
-import { Stack } from '@mui/material';
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Main } from "@/components/Styled";
+import isologo from "@/assets/branding/hola-isologo-coloful.svg";
+import bannerHome from "@/assets/images/banner-home.png";
+import footerHome from "@/assets/images/footer-home.png";
+import { Stack } from "@mui/material";
 
 export default function Login() {  
   return (
     <Main
       sx={{
-        backgroundColor: '#F7F7F7',
+        backgroundColor: "#F7F7F7",
       }}
     >
+
       <Grid 
         container  
         spacing={2}
@@ -30,8 +32,8 @@ export default function Login() {
             src={isologo}
             style={{
               height: 100,
-              paddingLeft: '60px',
-              paddingTop: '60px'
+              paddingLeft: "60px",
+              paddingTop: "60px"
             }}
           />
         </Grid>
@@ -47,75 +49,71 @@ export default function Login() {
               spacing={2}
               style={{
                 height: 100,
-                paddingTop: '60px',
-                paddingLeft: '60px'
+                paddingTop: "60px",
+                paddingLeft: "60px"
               }}
             >
-              <Button 
-                variant="text"
-                style={{
-                  color: '#C4C4C4',
-                }}
-                href="/Login"
-              >
-                Entrar
-              </Button>
-              <Button 
-                variant="outlined"
-                style={{ 
-                  color: '#1976D2',
-                }}
-                href="/request-access"
-              >
-                Solicitar Acesso
-              </Button>
+              <Link href="/Login">
+                <Button 
+                  variant="text"
+                  style={{
+                    color: "#C4C4C4",
+                  }}
+                >
+                  Entrar
+                </Button>
+              </Link>
+
+              <Link href="/request-access">
+                <Button 
+                  variant="outlined"
+                  style={{ 
+                    color: "#1976D2",
+                  }}
+                  href="/request-access"
+                >
+                  Solicitar Acesso
+                </Button>
+              </Link>
             </Stack>
         </Grid>
-
-
-          
       </Grid>
-
-
-
-
-
 
       <Grid 
         container  
         sx={{ 
-          minHeight: '100%'
+          minHeight: "100%"
         }}
       >
         <Grid 
           item 
           xs={4}
           sx={{ 
-            backgroundColor: '#F7F7F7',
+            backgroundColor: "#F7F7F7",
           }}
         >
           <Typography
-            variant='h5'
-            component='h1'
+            variant="h5"
+            component="h1"
             sx={{
-              paddingLeft: '60px',
-              fontSize: '50px',
-              fontWeight: 'bold',
-              paddingTop: '150px',
-              color: '#212121',
-              textAlign: 'left',
+              paddingLeft: "60px",
+              fontSize: "50px",
+              fontWeight: "bold",
+              paddingTop: "150px",
+              color: "#212121",
+              textAlign: "left",
             }}
           >
-            Tudo começa <br/> com um <span style={{ color: '#1976D2' }}>olá</span>
+            Tudo começa <br/> com um <span style={{ color: "#1976D2" }}>olá</span>
           </Typography>
           <Typography
-            variant='h5'
-            component='p'
+            variant="h5"
+            component="p"
             sx={{
-              paddingLeft: '60px',
-              fontSize: '20px',
-              color: '#212121',
-              textAlign: 'left',
+              paddingLeft: "60px",
+              fontSize: "20px",
+              color: "#212121",
+              textAlign: "left",
             }}
           >
             Venha se socializar com pessoas que <br /> gostam dos mesmos assuntos que você
@@ -128,21 +126,21 @@ export default function Login() {
               padding: 0,
               margin: 0,
               bottom: 0,
-              position: 'fixed',
+              position: "fixed",
             }}
           />
         </Grid>
         <Grid item xs={8}>
           <Box
-            component='img'
+            component="img"
             sx={{
-              width: '85%',
-              height: '85vh',
-              paddingBottom: '100px',
-              paddingLeft: '180px',
-              paddingTop: '50px',
-              objectFit: 'contain',
-              backgroundColor: '#F7F7F7',
+              width: "85%",
+              height: "85vh",
+              paddingBottom: "100px",
+              paddingLeft: "180px",
+              paddingTop: "50px",
+              objectFit: "contain",
+              backgroundColor: "#F7F7F7",
             }}
             src={bannerHome}
           />
