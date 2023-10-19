7a0a3fba8ff47c560bebd50dcdea1fc67300481b
Filipe Costa <costapfilipe@gmail.com>
2023-10-18 21:07:36 -0300

    Added Post component

---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,81 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import AvatarInfo from "@/components/AvatarInfo";
+import { ChatQuote } from "react-bootstrap-icons";
+import reaction1 from "@/assets/icons/reactions/1.svg";
+import reaction2 from "@/assets/icons/reactions/2.svg";
+import reaction3 from "@/assets/icons/reactions/3.svg";
+import reaction4 from "@/assets/icons/reactions/4.svg";
+import reaction5 from "@/assets/icons/reactions/5.svg";
+
+export default function Post() {
+  return (
+    <Box
+        sx={{
+            width: "100%",
+            height: "auto",
+            backgroundColor: "#fff",
+            border: "1px solid #c4c4c4",
+            borderRadius: "5px",
+            padding: "20px"
+        }}
+    >
+        <Grid container alignItems="center">
+            <Grid item xs={6}>
+                <AvatarInfo 
+                    avatarSize={35}
+                    title="Mark Alain"
+                    subtitle="Publicado em 03 set 2023"
+                />
+            </Grid>
+            <Grid item container xs={6} justifyContent="right">
+                <Stack spacing="8px" direction="row">
+                    <ChatQuote color="#777777" size={23}/>
+                    <Typography 
+                        component="span"
+                        fontSize={18}
+                        color="#777777"
+                    >
+                        Galáxias
+                    </Typography>
+                </Stack>
+            </Grid>
+        </Grid>
+
+        <Box sx={{ width: "100%", marginTop: "30px" }}>
+            <Typography 
+                component="span"
+                fontSize={16}
+                fontWeight="bold"
+                color="#404040"
+            >
+                Título da publicação
+            </Typography>
+            <Typography 
+                component="p"
+                fontSize={14}
+                color="#404040"
+                textAlign="justify"
+            >
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas quis erat sed massa lacinia accumsan sit amet sit amet quam. Morbi at ante sed turpis blandit molestie. Duis lacus nunc, rhoncus a mattis vel, imperdiet at sem. Ut in lorem ac nisl venenatis malesuada vel non massa. Maecenas nec fringilla eros, tempor ultricies risus. Nullam risus lacus, luctus non viverra ac, suscipit non magna. Ut quis risus varius, rutrum sem sit amet, ullamcorper nibh. Fusce tincidunt rutrum leo, eget posuere diam dictum eget. Mauris sollicitudin sem nec erat dictum, eget consectetur tortor pulvinar. Ut facilisis sagittis ante ac tincidunt. Sed porttitor interdum sodales. Nunc lacinia leo vitae sapien consectetur rutrum at eu risus. Ut mollis arcu non diam accumsan congue. 
+            </Typography>
+        </Box>
+        
+        <Grid container alignItems="center">
+            <Grid item xs={6}>
+                <Stack spacing="8px" direction="row">
+                    <Stack spacing="5px">
+                        
+                    </Stack>
+                </Stack>
+            </Grid>
+            <Grid item container xs={6} justifyContent="right">
+                
+            </Grid>
+        </Grid>
+
+    </Box>                                                 
+  );
+}
--- frontend/src/pages/Timeline.jsx
+++ frontend/src/pages/Timeline.jsx
@@ -1,13 +1,17 @@
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import AddPost from '@/components/AddPost';
-
+import Post from '@/components/Post';
 
 export default function Timeline() {
   return (
     <>
       <Grid container>
         <Grid item xs={8}>
-          <AddPost/>
+          <Stack spacing="28px">
+            <AddPost/>
+            <Post/>
+          </Stack>
         </Grid>
         <Grid item xs={4} sx={{ paddingLeft: "27px" }}>
 
