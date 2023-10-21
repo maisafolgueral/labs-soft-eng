00880289aa70f75a083dc03fbefbb7fc00bf6ffb
Filipe Costa <costapfilipe@gmail.com>
2023-10-20 22:32:58 -0300

    Added AnotherProfile page

--- frontend/src/pages/AnotherProfile.jsx
+++ frontend/src/pages/AnotherProfile.jsx
@@ -1,7 +1,24 @@
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Post from '@/components/Post';
+import InfoBox from "@/components/InfoBox";
+
+
 export default function AnotherProfile() {
   return (
     <>
-      
+      <Grid container>
+        <Grid item xs={8}>
+          <Stack spacing="28px">
+            {Array.from(Array(10)).map((_, index) => (
+              <Post/>
+            ))}
+          </Stack>
+        </Grid>
+        <Grid item xs={4} sx={{ paddingLeft: "27px" }}>
+          <InfoBox/>
+        </Grid>
+      </Grid>
     </>
   );
 }
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,4 +1,6 @@
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Post from '@/components/Post';
 import InfoBox from "@/components/InfoBox";
 
 
@@ -7,7 +9,11 @@ export default function Profile() {
     <>
       <Grid container>
         <Grid item xs={8}>
-          
+          <Stack spacing="28px">
+            {Array.from(Array(10)).map((_, index) => (
+              <Post/>
+            ))}
+          </Stack>
         </Grid>
         <Grid item xs={4} sx={{ paddingLeft: "27px" }}>
           <InfoBox/>
