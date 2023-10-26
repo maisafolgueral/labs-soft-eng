3ea75cd2fd83038cd979586537884a4aabb1d5ec
Matheus <matheusfarias519@gmail.com>
2023-10-25 22:31:29 -0300

    Feedback routes added

--- backend/routes/feedback.py
+++ backend/routes/feedback.py
@@ -15,5 +15,40 @@ session = sessionmaker(bind=engine)()
 
 @feedback_bp.route('/feedbacks', methods=["POST"])
 def sendFeedback():
-    # todo
-    return 'todo'
+    try:
+        # Received data
+        data = request.get_json()
+
+        # Validate data
+        FeedbackSchema().load(data)
+
+        # Persist data into the database
+        session.add(FeedbackModel(**data))
+        session.commit()
+            
+        return jsonify({
+            'code': 201,
+            'description': 'Successfully created'
+        })
+    
+    except ValidationError as err:
+        abort(400, err.messages)
+    except:
+        session.rollback()
+        abort(500)
+
+
+@feedback_bp.route('/feedbacks/<id>', methods=["GET"])
+def getFeedback(id):
+    try:
+        feedback = session.query(FeedbackModel).filter_by(id=id).first()
+        if feedback is None:
+            raise NoResultFound('Post not found')
+        
+        result = FeedbackSchema().dump(feedback)
+        return jsonify(result)
+    
+    except NoResultFound as err:
+        abort(404, err.args)
+    except:
+        abort(500)
--- backend/schemas.py
+++ backend/schemas.py
@@ -85,6 +85,6 @@ class Comment(Schema):
 class Feedback(Schema):
     id = fields.Integer(dump_only=True)
     user_id = fields.Integer(required=True)
-    subject = fields.String(required=True, validate=[validate.Length(min=20, max=100)])
+    subject = fields.String(required=True, validate=[validate.Length(min=10, max=100)])
     description = fields.String(required=True, validate=[validate.Length(min=30, max=500)])
     created_at = fields.DateTime(dump_only=True)
