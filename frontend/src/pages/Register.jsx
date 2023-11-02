9043e182cfebd00f78a5533e5d4e7b3aff102990
Filipe Costa <costapfilipe@gmail.com>
2023-11-01 22:56:28 -0300

    Added form validation to register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 import { useNavigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
@@ -24,16 +26,41 @@ import isologo from "@/assets/branding/hola-isologo-coloful.svg";
 import bannerConversation from "@/assets/images/banner-conversation.png";
 
 
+const validationSchema = yup.object({
+  name: yup
+    .string('Preencha o seu nome')
+    .required('Nome é obrigatório'),
+  surname: yup
+    .string('Preencha o seu sobrenome')
+    .required('Sobrenome é obrigatório'),
+  birthday: yup
+    .string('Preencha o seu aniversário')
+    .required('Aniversário é obrigatório'),
+  gender: yup
+    .string('Selecione o seu sexo')
+    .required('Sexo é obrigatório')
+    .oneOf(['M', 'F']),
+  email: yup
+    .string('Preencha o seu email')
+    .email('Email invalido')
+    .required('Email é obrigatório'),
+  password: yup
+    .string('Preencha a sua senha')
+    .min(6, 'Senha deve conter no mínimo 6 caracteres')
+    .max(12, 'Senha deve conter no máximo 12 caracteres')
+    .required('Senha é obrigatório'),
+});
+
 function Form() {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   const [alert, setAlert] = React.useState(false);
   const [alertMessage, setAlertMessage] = React.useState('');
   const [loading, setLoading] = React.useState(false);
-  const [name, setName] = React.useState("");
+  /*const [name, setName] = React.useState("");
   const [surname, setSurname] = React.useState("");
   const [birthday, setBirthday] = React.useState("");
   const [gender, setGender] = React.useState("");
-  const [email, setEmail] = React.useState("");
+  const [email, setEmail] = React.useState("");*/
   const [passwords, setPasswords] = React.useState({
     password1: "",
     password2: "",
@@ -41,7 +68,7 @@ function Form() {
     password2Visible: false,
   });
 
-  const handleBirthday = (value) => {
+  /*const handleBirthday = (value) => {
     const date = new Date(value); 
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, '0');
@@ -49,14 +76,13 @@ function Form() {
     const formattedDate = `${yyyy}-${mm}-${dd}`;
 
     setBirthday(formattedDate);
-  };
+  };*/
   
   const handlePasswordChange = (field) => (event) => {
     setPasswords({
       ...passwords,
       [field]: event.target.value,
     });
-    // Password verify
   };
 
   const togglePasswordVisibility = (field) => () => {
@@ -70,7 +96,22 @@ function Form() {
     event.preventDefault();
   };
 
-  let handleSubmit = async (e) => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      surname: '',
+      birthday: '',
+      gender: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
+
+  /*let handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
@@ -106,60 +147,79 @@ function Form() {
     } finally {
       setLoading(false);
     }
-  };
+  };*/
 
   return (
     <Box
       component="form"
       noValidate
       autoComplete="off"
-      onSubmit={handleSubmit}
+      onSubmit={formik.handleSubmit}
     >
       <Stack direction="column" spacing={2}>
 
-        {alert &&
+        {/*alert &&
         <Alert 
           variant="filled" 
           severity="error"
         >
           { alertMessage }
-        </Alert>
+        </Alert>*/
         }
 
         <TextField 
           fullWidth  
+          name="name"
           label="Nome" 
           variant="outlined"
           size="small"
-          onChange={(e) => setName(e.target.value)}
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
         />
 
         <TextField 
           fullWidth  
+          name="surname"
           label="Sobrenome" 
           variant="outlined"
           size="small"
-          onChange={(e) => setSurname(e.target.value)}
+          value={formik.values.surname}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.surname && Boolean(formik.errors.surname)}
+          helperText={formik.touched.surname && formik.errors.surname}
         />
 
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt">
-            <DatePicker  
-                label="Aniversário" 
-                variant="outlined" 
-                format="DD/MM/YYYY"
-                slotProps={{ textField: { size: "small" } }}
-                onChange={handleBirthday}
-            />
+          <DatePicker  
+            name="birthday"
+            label="Aniversário" 
+            variant="outlined" 
+            format="DD/MM/YYYY"
+            slotProps={{ textField: { size: "small" } }}
+            value={formik.values.birthday}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.birthday && Boolean(formik.errors.birthday)}
+            helperText={formik.touched.birthday && formik.errors.birthday}
+          />
         </LocalizationProvider>
 
         <TextField
-          value={gender}
           fullWidth 
           select 
+          name="gender"
           label="Sexo" 
           variant="outlined"
           size="small"
-          onChange={(e) => setGender(e.target.value)}
+          value={formik.values.gender}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.gender && Boolean(formik.errors.gender)}
+          helperText={formik.touched.gender && formik.errors.gender}
         >
           <MenuItem key={1} value="M">
             Masculino
@@ -171,10 +231,15 @@ function Form() {
 
         <TextField 
           fullWidth  
+          name="email"
           label="E-mail" 
           variant="outlined"
           size="small"
-          onChange={(e) => setEmail(e.target.value)}
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
         />
 
         <FormControl variant="outlined" size="small">
