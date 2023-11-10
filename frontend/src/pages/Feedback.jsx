34c138e9febdb1a1c1953f50c58e201d5de7db4f
Maisa Folgueral <maisa.folgueral@gmail.com>
2023-11-10 09:58:35 -0300

    Added formik on feedback

--- frontend/src/pages/Feedback.jsx
+++ frontend/src/pages/Feedback.jsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import FormControl from '@mui/material/FormControl';
@@ -6,16 +8,42 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import LoadingButton from '@mui/lab/LoadingButton';
+import Alert from "@mui/material/Alert";
 import { Icon } from "@/components/Icon";
 
+const validationSchema = yup.object({
+    subject: yup.string()
+      .required("Título é obrigatório"),
+    description: yup.string()
+      .required("Descrição é obrigatório"),
+  });
+  
 
 export default function Feedback() {
     const [loading, setLoading] = React.useState(false);
+    const [alert, setAlert] = React.useState(false);
+    const [alertMessage, setAlertMessage] = React.useState("");
+  
+    let displayError = (message) => {
+      setAlert(true);
+      setAlertMessage(message);
+    }
 
-    const submitHandler = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
         setLoading(true);
     }
+
+    const formik = useFormik({
+        initialValues: {
+          subject: "",
+          description: "",
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+          handleSubmit(values);
+        },
+    });
 
     return (
         <>
@@ -23,7 +51,7 @@ export default function Feedback() {
                 component="form"
                 noValidate
                 autoComplete="off"
-                onSubmit={submitHandler}
+                onSubmit={formik.handleSubmit}
                 sx={{
                     width: "100%",
                     height: "auto",
@@ -56,17 +84,37 @@ export default function Feedback() {
                     <Grid item xs={6}>
                         <FormControl fullWidth>
                             <Stack spacing="21px">
+                                {alert &&
+                                    <Alert 
+                                    variant="filled" 
+                                    severity="error"
+                                    >
+                                        { alertMessage }
+                                    </Alert>
+                                }
                                 <TextField 
-                                    label="Qual o motivo?" 
+                                    name="subject"
+                                    label="Título" 
                                     variant="outlined" 
                                     size="small"
+                                    value={formik.values.subject}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.subject && Boolean(formik.errors.subject)}
+                                    helperText={formik.touched.subject && formik.errors.subject}
                                 />
                                 <TextField
-                                    label="Faça uma breve descrição do motivo" 
+                                    name="description"
+                                    label="Descrição" 
                                     multiline
                                     rows={10}
                                     variant="outlined" 
                                     size="small"
+                                    value={formik.values.description}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.description && Boolean(formik.errors.description)}
+                                    helperText={formik.touched.description && formik.errors.description}
                                 />
                                 <Stack 
                                     direction="row" 
@@ -78,7 +126,7 @@ export default function Feedback() {
                                         size="medium"
                                         spacing={2}
                                         loading={loading}
-                                        disabled
+                                        disabled={!formik.dirty}
                                     >
                                         Enviar
                                     </LoadingButton>
