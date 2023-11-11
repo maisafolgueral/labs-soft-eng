c459f2138323fd0545ab933ab68b8e8d8ad9cc67
maisafolgueral <maisa.folgueral@gmail.com>
2023-11-11 18:49:15 -0300

    Added form validation to EditProfile

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
 import PropTypes from 'prop-types';
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
@@ -6,6 +8,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
+import Alert from "@mui/material/Alert";
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Select from '@mui/material/Select';
@@ -23,8 +26,25 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import dayjs from 'dayjs';
 import "dayjs/locale/pt";
 import { Icon } from "@/components/Icon";
+import PasswordInput from "@/components/PasswordInput";
 
 
+const calculateAge = (birthdate) => {
+    const birthDate = new Date(birthdate);
+    const currentDate = new Date();
+    
+    const yearsDiff = currentDate.getFullYear() - birthDate.getFullYear();
+    const monthsDiff = currentDate.getMonth() - birthDate.getMonth();
+    const daysDiff = currentDate.getDate() - birthDate.getDate();
+    
+    // Adjust the age if the birthdate for the current year hasn't occurred yet
+    if (monthsDiff < 0 || (monthsDiff === 0 && daysDiff < 0)) {
+      return yearsDiff - 1;
+    } else {
+      return yearsDiff;
+    }
+}
+  
 function CustomTabPanel(props) {
     const { children, value, index, ...other } = props;
   
@@ -99,11 +119,70 @@ function ControlTabs({ currentTab, handleChangeTab }) {
 }
 
 function PersonalTab({ currentTab }) {
-    const [loading, setLoading] = React.useState(false);
+    const validationSchema = yup.object({
+        name: yup.string()
+            .required("Nome é obrigatório"),
+        surname: yup.string()
+            .required("Sobrenome é obrigatório"),
+        birthday: yup.string()
+            .required("Aniversário é obrigatório")
+            .test(
+            "age-range",
+            "Idade não permitida",
+            (value) => {
+                let age = calculateAge(value);
+                return age >= 14 && age <= 30;
+            }
+            ),
+        gender: yup.string()
+            .oneOf(["M", "F"])
+            .required("Sexo é obrigatório"),
+        email: yup.string()
+            .email("E-mail inválido")
+            .required("E-mail é obrigatório")
+    });
 
-    const submitHandler = () => {
-        setLoading(true);
+    const [loading, setLoading] = React.useState(false);
+    const [alert, setAlert] = React.useState(false);
+    const [alertMessage, setAlertMessage] = React.useState("");
+  
+    const handleBirthday = (value) => {
+      let outputDate = null;
+      
+      if(value) {
+        const date = new Date(value); 
+        let yyyy = date.getFullYear();
+        let mm = String(date.getMonth() + 1).padStart(2, "0");
+        let dd = String(date.getDate()).padStart(2, "0");
+        outputDate = `${yyyy}-${mm}-${dd}`;
+      }
+      
+      formik.setFieldValue('birthday', outputDate);
     };
+  
+    let displayError = (message) => {
+      setAlert(true);
+      setAlertMessage(message);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setLoading(true);
+    }
+
+    const formik = useFormik({
+        initialValues: {
+          name: "Marie",
+          surname: "Canon",
+          birthday: "",
+          gender: "F",
+          email: "marie.canon@gmail.com",
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+          handleSubmit(values);
+        },
+      });
 
     return (
         <CustomTabPanel value={currentTab} index={0}>
@@ -111,58 +190,90 @@ function PersonalTab({ currentTab }) {
                 component="form"
                 noValidate
                 autoComplete="off"
-                onSubmit={submitHandler}
+                onSubmit={formik.handleSubmit}
             >
                 <Stack spacing="28px">
+                    {alert &&
+                    <Alert 
+                        variant="filled" 
+                        severity="error"
+                    >
+                        { alertMessage }
+                    </Alert>
+                    }
                     <TextField 
                         id="name" 
-                        label="Nome" 
                         variant="outlined" 
-                        defaultValue="Marie"
                         size="small"
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.name && Boolean(formik.errors.name)}
+                        helperText={formik.touched.name && formik.errors.name}
                     />
 
                     <TextField 
-                        id="lastname" 
-                        label="Sobrenome" 
+                        id="surname" 
                         variant="outlined" 
-                        defaultValue="Canon"
                         size="small"
+                        value={formik.values.surname}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.surname && Boolean(formik.errors.surname)}
+                        helperText={formik.touched.surname && formik.errors.surname}
                     />
 
-                    <LocalizationProvider 
-                        dateAdapter={AdapterDayjs} 
-                        adapterLocale="pt"
-                    >
-                        <DatePicker 
-                            id="birthday" 
-                            label="Aniversário" 
-                            variant="outlined" 
-                            format="DD/MM/YYYY"
-                            slotProps={{ 
-                                textField: { size: 'small' } 
-                            }}
-                            defaultValue={dayjs('2022-04-25')}
-                        />
+
+                    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt">
+                            <DatePicker  
+                                id="birthday"
+                                name="birthday"
+                                variant="outlined" 
+                                format="DD/MM/YYYY"
+                                onChange={handleBirthday}
+                                defaultValue={dayjs('2022-04-25')}
+                                slotProps={{
+                                textField: {
+                                    size: "small",
+                                    selected: formik.values.birthday,
+                                    error: formik.touched.birthday && Boolean(formik.errors.birthday),
+                                    helperText: formik.touched.birthday && formik.errors.birthday,
+                                    onBlur: formik.handleBlur,
+                                }
+                                }}
+                            />
                     </LocalizationProvider>
 
-                    <FormControl size="small" disabled defaultValue="F">
-                        <InputLabel>Sexo</InputLabel>
-                        <Select
-                            value="F"
-                            label="Sexo"
-                        >
-                            <MenuItem value="M">Masculino</MenuItem>
-                            <MenuItem value="F">Feminino</MenuItem>
-                        </Select>
-                    </FormControl>
+                    <TextField
+                        fullWidth 
+                        select 
+                        name="gender"
+                        label="Sexo" 
+                        variant="outlined"
+                        size="small"
+                        value={formik.values.gender}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.gender && Boolean(formik.errors.gender)}
+                        helperText={formik.touched.gender && formik.errors.gender}
+                    >
+                        <MenuItem key={1} value="M">
+                            Masculino
+                        </MenuItem>
+                        <MenuItem key={2} value="F">
+                            Feminino
+                        </MenuItem>
+                    </TextField>
 
                     <TextField 
                         id="email" 
-                        label="E-mail" 
                         variant="outlined" 
-                        defaultValue="marie.canon@gmail.com"
                         size="small"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
                     />
 
                     <Stack 
@@ -175,7 +286,8 @@ function PersonalTab({ currentTab }) {
                             size="medium"
                             spacing={2}
                             loading={loading}
-                            disabled
+                            onClick={handleSubmit}
+                            disabled={!formik.dirty}
                         >
                             Atualizar
                         </LoadingButton>
@@ -187,120 +299,106 @@ function PersonalTab({ currentTab }) {
 }
 
 function SecurityTab({ currentTab }) {
-    const [loading, setLoading] = React.useState(false);
-
-    const [passwords, setPasswords] = React.useState({
-        password1: 'senhaatual',
-        password2: 'novasenha',
-        password3: 'novasenha',
-        password1Visible: false,
-        password2Visible: false,
-        password3Visible: false,
+    const validationSchema = yup.object({
+        password: yup.string()
+        .min(6, "Senha deve conter no mínimo 6 caracteres")
+        .max(12, "Senha deve conter no máximo 12 caracteres")
+        .required("Senha é obrigatório"),
+        newpassword: yup.string()
+          .min(6, "Senha deve conter no mínimo 6 caracteres")
+          .max(12, "Senha deve conter no máximo 12 caracteres")
+          .required("Senha é obrigatório"),
+        passwordConfirmation: yup.string()
+          .oneOf([yup.ref('newpassword')], 'As senhas devem ser iguais')
+          .required("Confirmar a senha é obrigatório"),
     });
-    
-    const handlePasswordChange = (field) => (event) => {
-        setPasswords({
-            ...passwords,
-            [field]: event.target.value,
-        });
-    };
-    
-    const togglePasswordVisibility = (field) => () => {
-        setPasswords({
-            ...passwords,
-            [field]: !passwords[field],
-        });
-    };
 
-    const handleMouseDownPassword = (event) => {
+    const [loading, setLoading] = React.useState(false);
+    const [alert, setAlert] = React.useState(false);
+    const [alertMessage, setAlertMessage] = React.useState("");
+   
+    let displayError = (message) => {
+      setAlert(true);
+      setAlertMessage(message);
+    }
+
+    const handleSubmit = (event) => {
         event.preventDefault();
-    };
-  
-    const submitHandler = () => {
         setLoading(true);
-    };
+    }
+
+    const formik = useFormik({
+        initialValues: {
+          password: "senhaatual",
+          newpassword: "novasenha",
+          passwordConfirmation: "novasenha",
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+          handleSubmit(values);
+        },
+    });
 
     return (
-        <CustomTabPanel value={currentTab} index={1}> 
+        <CustomTabPanel value={currentTab} index={1}>
             <Box
                 component="form"
                 noValidate
                 autoComplete="off"
-                onSubmit={submitHandler}
+                onSubmit={formik.handleSubmit}
             >
                 <Stack spacing="28px">
-                    <FormControl variant="outlined" size="small">
-                        <InputLabel htmlFor="outlined-adornment-password">Senha atual</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            label="Senha atual"
-                            type={passwords.password1Visible ? 'text' : 'password'}
-                            value={passwords.password1}
-                            onChange={handlePasswordChange('password1')}
-                            endAdornment={
-                                <InputAdornment position="end" >
-                                    <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={togglePasswordVisibility('password1Visible')}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                    >
-                                    {passwords.password1Visible ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
+                    {alert &&
+                    <Alert 
+                        variant="filled" 
+                        severity="error"
+                    >
+                        { alertMessage }
+                    </Alert>
+                    }
+                        <PasswordInput
+                            fullWidth  
+                            name="password"
+                            label="Senha" 
+                            variant="outlined"
+                            size="small"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.password && Boolean(formik.errors.password)}
+                            helperText={formik.touched.password && formik.errors.password}
                         />
-                    </FormControl>
 
-                    <FormControl variant="outlined" size="small">
-                        <InputLabel htmlFor="outlined-adornment-password">Nova senha</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            label="Nova senha"
-                            type={passwords.password2Visible ? 'text' : 'password'}
-                            value={passwords.password2}
-                            onChange={handlePasswordChange('password2')}
-                            endAdornment={
-                                <InputAdornment position="end" >
-                                    <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={togglePasswordVisibility('password2Visible')}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                    >
-                                    {passwords.password2Visible ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
+                        <PasswordInput
+                            fullWidth  
+                            name="newpassword"
+                            label="Senha" 
+                            variant="outlined"
+                            size="small"
+                            value={formik.values.newpassword}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.newpassword && Boolean(formik.errors.newpassword)}
+                            helperText={formik.touched.newpassword && formik.errors.newpassword}
                         />
-                    </FormControl>
 
-                    <FormControl variant="outlined" size="small">
-                        <InputLabel htmlFor="outlined-adornment-password">Repita a nova senha</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            label="Repita a nova senha"
-                            type={passwords.password3Visible ? 'text' : 'password'}
-                            value={passwords.password3}
-                            onChange={handlePasswordChange('password3')}
-                            endAdornment={
-                                <InputAdornment position="end" >
-                                    <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={togglePasswordVisibility('password3Visible')}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                    >
-                                    {passwords.password3Visible ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
+                        <PasswordInput
+                            fullWidth  
+                            name="passwordConfirmation"
+                            label="Confirmar senha" 
+                            variant="outlined"
+                            size="small"
+                            type="password"
+                            value={formik.values.passwordConfirmation}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.passwordConfirmation && Boolean(formik.errors.passwordConfirmation)}
+                            helperText={formik.touched.passwordConfirmation && formik.errors.passwordConfirmation}
                         />
-                    </FormControl>
 
                     <Stack 
-                        direction="row" 
-                        spacing={2} 
+                        direction="row"
+                        spacing={2}
                         justifyContent="right"
                     >
                         <LoadingButton 
@@ -308,7 +406,8 @@ function SecurityTab({ currentTab }) {
                             size="medium"
                             spacing={2}
                             loading={loading}
-                            disabled
+                            onClick={handleSubmit}
+                            disabled={!formik.dirty}
                         >
                             Atualizar
                         </LoadingButton>
